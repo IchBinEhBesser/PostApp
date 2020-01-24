26e26215503df5c22ff83a3ecebb605f78b10151
Maxi M <maximilianeias.meier@web.de>
2020-01-24 17:21:00 +0100

    update

--- app/src/main/java/com/example/post/LoginActivity.java
+++ app/src/main/java/com/example/post/LoginActivity.java
@@ -1,9 +1,19 @@
 package com.example.post;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
+
+    public TextView registerLink;
+    public EditText usernameLEditText;
+    public EditText passwordLEditText;
+    public Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

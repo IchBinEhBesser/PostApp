82eff458776356705f5c1daf5f1388006820f7f5
Maxi M <maximilianeias.meier@web.de>
2020-01-24 18:16:00 +0100

    update

--- app/src/main/java/com/example/post/LoginActivity.java
+++ app/src/main/java/com/example/post/LoginActivity.java
@@ -2,10 +2,13 @@ package com.example.post;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
@@ -20,15 +23,22 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        registerLink = findViewById(R.id.tvRegisteHere);
-        usernameLEditText = findViewById(R.id.etUsername);
-        passwordLEditText = findViewById(R.id.etPassword);
-        loginButton = findViewById(R.id.bLogin);
+        registerLink = findViewById(R.id.tvLRegisteHere);
+        usernameLEditText = findViewById(R.id.etLUsername);
+        passwordLEditText = findViewById(R.id.etLPassword);
+        loginButton = findViewById(R.id.bLLogin);
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 login(usernameLEditText.getText().toString(),passwordLEditText.getText().toString());
+            }
+        });
+
+        registerLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registrade(usernameLEditText.getText().toString(),passwordLEditText.getText().toString());
             }
         });
 
@@ -39,8 +49,40 @@ public class LoginActivity extends AppCompatActivity {
         Intent loginIntent = new Intent(LoginActivity.this, MainActivity.class);
 
         startActivity(loginIntent);
+        onResume();
 
     }
 
+    public void registrade(String uname, String password){
 
+        usernameLEditText.setText(uname);
+
+        passwordLEditText.setText(password);
+        Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+
+        startActivity(registerIntent);
+        onPause();
+
+    }
+
+    protected void onCreate(Bundle savedInstanceState,String email, String password) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        usernameLEditText.setText(email);
+        passwordLEditText.setText(password);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    login(usernameLEditText.getText().toString(),passwordLEditText.getText().toString());
+                }catch(Exception e){
+                    Toast.makeText(LoginActivity.this, "Anmeldung fehlgeschlagen", Toast.LENGTH_SHORT).show();
+                    Intent loginIntent = new Intent(LoginActivity.this, LoginActivity.class);
+                }
+
+            }
+        });
+    }
 }
--- app/src/main/java/com/example/post/RegisterActivity.java
+++ app/src/main/java/com/example/post/RegisterActivity.java
@@ -2,13 +2,43 @@ package com.example.post;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    public EditText usernameREditText;
+    public EditText passwordREditText;
+    public EditText emailREditText;
+    public Button registerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+
+        usernameREditText = findViewById(R.id.etRUsername);
+        emailREditText = findViewById(R.id.etREmail);
+        passwordREditText = findViewById(R.id.etRPassword);
+        registerButton = findViewById(R.id.bRLogin);
+
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registrade(emailREditText.getText().toString(), passwordREditText.getText().toString());
+            }
+        });
+    }
+
+
+    public void registrade(String uname, String password) {
+        Intent registerIntent = new Intent(RegisterActivity.this, LoginActivity.class);
+        startActivity(registerIntent);
+
     }
 }

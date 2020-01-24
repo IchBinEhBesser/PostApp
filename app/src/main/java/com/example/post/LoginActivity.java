fc49c58f29d1bb9f5a294414e23f94fbfb01e8fa
Maxi M <maximilianeias.meier@web.de>
2020-01-24 16:22:33 +0100

    update

--- app/src/main/java/com/example/post/LoginActivity.java
+++ app/src/main/java/com/example/post/LoginActivity.java
@@ -1,35 +1,18 @@
 package com.example.post;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
-import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
-    public Button loginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        loginButton.findViewById(R.id.bLogin);
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openMainActivity();
-            }
-        });
+
     }
 
 
-    public void openMainActivity(){
-        Intent mainIntent = new Intent(LoginActivity.this, MainActivity.class);
-        startActivity(mainIntent);
-        onPause();
-        onDestroy();
-    }
+
 }
--- app/src/main/java/com/example/post/MapActivity.java
+++ app/src/main/java/com/example/post/MapActivity.java
@@ -1,14 +1,24 @@
 package com.example.post;
+import android.os.Bundle;
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.maps.Style;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MapActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_map);
+
     }
 }
+
